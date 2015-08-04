d3ea7ae0e46e62c6eef73475e1558d16edc29a46
nutsdo <ldingtao@163.com>
2015-08-04 14:13:16 +0800

    测试回复

--- app/Http/Controllers/Wechat/WechatController.php
+++ app/Http/Controllers/Wechat/WechatController.php
@@ -52,15 +52,16 @@ class WechatController extends Controller{
         //return Message::make('text')->content('欢迎关注！');
         //获取公众号信息
         $public_number = $message->ToUserName;
+
+        return Message::make('text')->content($message->Content);
         $wechat = Wechat::where('wechat_account','=',$public_number)->firstOrFail();
         //获取关键词对象
         //查询关键字,预载入关键字规则
-//        $keyword = Keyword::with(['keywordRule'=>function($query) use ($wechat){
-//            $query->where('wechat_id','=',$wechat->id);
-//        }])->whereRaw('wechat_id = ? and keyword like ? ',[$wechat->id,$message->Content])->firstOrFail();
+        $keyword = Keyword::with(['keywordRule'=>function($query) use ($wechat){
+            $query->where('wechat_id','=',$wechat->id);
+        }])->whereRaw('wechat_id = ? and keyword like ? ',[$wechat->id,$message->Content])->firstOrFail();
 
         //查询对应回复   一对多
-        return Message::make('text')->content($message->Content);
         $replies = $keyword->keywordRule->reply;
 
         foreach ($replies as $key => $reply) {
